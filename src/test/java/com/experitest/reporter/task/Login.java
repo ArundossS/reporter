6f0aa489578f2599089c1e82c30c379dc7fc3824
Arundoss <arundoss_p@experitest.com>
2017-10-12 01:07:28 +0300

    if-else

--- src/test/java/com/experitest/reporter/task/Login.java
+++ src/test/java/com/experitest/reporter/task/Login.java
@@ -3,6 +3,7 @@ package com.experitest.reporter.task;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+
 import org.testng.annotations.Test;
 import com.experitest.reporter.task.TestBase;
 
@@ -12,12 +13,13 @@ public class Login extends TestBase {
 	public void login() {
 		Random rand = new Random();
 		int  n = rand.nextInt(10);
-
+		driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("company");
+		driver.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("company");
 		if (n<=5){
-			driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("company");
-			driver.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("company");
+
 			driver.findElement(By.xpath("//*[@text='Login']")).click();	
-			driver.findElement(By.xpath("//*[@text='Logout']")).click();
+
 		}
+		driver.findElement(By.xpath("//*[@text='Logout']")).click();
 	}
 }
